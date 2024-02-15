fd101bcead8d64659e85f521e08dfd7711f12d04
허재영 <jy0831@brothersb.com>
2024-02-15 15:39:42 +0900

    버전관리, 리드미 수정

--- README.md
+++ README.md
@@ -1,35 +1,21 @@
-# Dino
+# PlayGround
 
-This template should help get you started developing with Vue 3 in Vite.
-
-## Recommended IDE Setup
-
-[VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vitejs.dev/config/).
+이것저것 시도해보고 재밌어 보이는거 만들어 보는곳
 
 ## Project Setup
 
 ```sh
-npm install
+yarn install
 ```
 
 ### Compile and Hot-Reload for Development
 
 ```sh
-npm run dev
+yarn dev
 ```
 
 ### Compile and Minify for Production
 
 ```sh
-npm run build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-npm run lint
+yarn build
 ```
--- src/router/index.js
+++ src/router/index.js
@@ -12,49 +12,31 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     },
     {
       path: '/grid',
       name: 'GridTest',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route NewPage
-      // which is lazy-loaded when the route is visited.
       component: GridTest
     },
     {
       path: '/new',
       name: 'NewPage',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route NewPage
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/NewPage.vue')
     },
     {
       path: '/banner',
       name: 'AniBanner',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route NewPage
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AniBanner.vue')
     },
     {
       path: '/canvas',
       name: 'CanvasView',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route NewPage
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/CanvasView.vue')
     },
     {
       path: '/socket',
       name: 'WebSocket',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route NewPage
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/WebSocket.vue')
     }
   ]
